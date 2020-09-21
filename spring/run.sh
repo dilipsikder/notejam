2a3a7587a4e18c83b23c7db0f69ea5bf38d4157b
dilipsikder123 <sikdar.dilip@gmail.com>
2020-09-21 20:29:02 +0200

    Changes done for app.yaml
    Changes done for app.yaml

--- spring/run.sh
+++ spring/run.sh
@@ -1,15 +1,9 @@
 #!/bin/sh
 
 # Start the proxy
-./cloud_sql_proxy -instances=notejam1:mysql1=tcp:3306 
+nohup ./cloud_sql_proxy -instances=notejam1:mysql1=tcp:3306 >/dev/null 2>&1 &
 
 # wait for the proxy to spin up
 sleep 10
 
-# Start the server
-./server
-
-
-
-
-
+/usr/bin/java -jar spring-0.0.1-SNAPSHOT.jar
